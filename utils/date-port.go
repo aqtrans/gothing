61b891d1c02ed6813c8dc846bb8e5ce78a76f606
Jordan Anderson <me@jba.io>
2015-02-14 22:15:37 -0600

    date-port.go removed from main pkg

--- utils/date-port.go
+++ utils/date-port.go
@@ -1,4 +1,4 @@
-package migrate
+package main
 
 //This file was used by me to reformat the dates on already-uploaded files, from a stupid string-based thing, to a Unix epoch timestamp
 //I'm now using a template funcMap to take the Unix timestamp and properly format it when printed
