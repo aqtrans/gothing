3ac7a57072b7f2e85073489e54d9067929a0769a
Jordan Anderson <me@jba.io>
2016-03-06 13:14:59 -0600

    Added a small utility to update schema in boltdb.

---
+++ utils/shorturl-port.go
@@ -0,0 +1,59 @@
+package main
+
+//This file was used by me to change the "schema" of existing ShortURLs
+// There is now a FullURL field which needs to be populated
+
+import (
+	"encoding/json"
+	"github.com/boltdb/bolt"
+	"log"
+)
+
+type OShorturl struct {
+	Created int64
+	Short 	string
+	Long 	string
+	Hits 	int64
+}
+
+type Shorturl struct {
+	Created int64
+	Short   string
+    FullURL string
+	Long    string
+	Hits    int64
+}
+
+func main() {
+
+// Open the database.
+Db, _ := bolt.Open("./bolt.db", 0666, nil)
+defer Db.Close()
+
+//Lets try this with boltDB now!
+Db.Update(func(tx *bolt.Tx) error {
+
+    shorts := tx.Bucket([]byte("Shorturls"))
+    shorts.ForEach(func(k, v []byte) error {
+    	//log.Println("FILES: key="+string(k)+" value="+string(v))
+        //fmt.Printf("key=%s, value=%s\n", k, v)
+        var short *OShorturl
+        err := json.Unmarshal(v, &short)
+		if err != nil {
+			log.Println(err)
+		}
+        fullUrl := "http://" + short.Short + "." + "es.gy"
+        sh := &Shorturl{
+            Created: short.Created,
+            Short: short.Short,
+            Long: short.Long,
+            FullURL: fullUrl,
+            Hits: short.Hits,
+        }
+        encoded, err := json.Marshal(sh)
+		return shorts.Put(k, encoded)
+    })            
+    return nil
+})
+	
+}
