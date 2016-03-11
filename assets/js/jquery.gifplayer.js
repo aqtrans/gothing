29d580e35933588d76fa86ee339952c3c9b32d63
Jordan Anderson <me@jba.io>
2016-03-11 16:44:14 -0600

    Updated gifplayer.js, should handle mp4 and webm's now!

--- assets/js/jquery.gifplayer.js
+++ assets/js/jquery.gifplayer.js
@@ -1,117 +1,276 @@
 /* 
- * Gifplayer v0.1.5
- * Customizable jquery plugin to play and stop animated gifs. Similar to 9gag's
- * (c)2014 Rubén Torres - rubentdlh@gmail.com
- * Released under the MIT license
- */
+* Gifplayer v0.2.2
+* Customizable jquery plugin to play and stop animated gifs. Similar to 9gag's
+* (c)2014 RubÃ©n Torres - rubentdlh@gmail.com
+* Released under the MIT license
+*/
 
- (function($) {
+(function($) {
 
- 	function GifPlayer(preview, options){
- 		this.previewElement=preview;
- 		this.spinnerElement=$("<div class = 'spinner'></div>");
- 		this.options=options;
- 		this.gifLoaded=false;
- 	}
+	function GifPlayer(preview, options){
+		this.previewElement = preview;
+		this.options = options;
+		this.animationLoaded = false;
+	}
 
- 	GifPlayer.prototype = {
+	GifPlayer.scopes = new Array();
 
- 		activate: function(){
- 			this.wrap();
- 			this.addSpinner();
- 			this.addControl();
- 			this.addEvents();
- 		},
+	GifPlayer.prototype = {
 
- 		wrap: function(){
- 			this.wrapper = this.previewElement.wrap("<div class='gifplayer-wrapper'></div>").parent();
- 			this.wrapper.css('max-width', '300');
- 			this.wrapper.css('max-height', '300');
- 			this.previewElement.addClass('gifplayer');
- 			this.previewElement.css('cursor','pointer');
- 		},
+		supportedFormats: ['gif', 'jpeg', 'jpg', 'png'],
 
- 		getGifSrc: function(){
- 			var size = "-" + this.previewElement.width() + 'x' + this.previewElement.height();
- 			var linkHref = 	this.previewElement.attr('src').replace('.png','').replace('thumbs','imagedirect');
- 			return linkHref;
- 		},
+		activate: function(){
+			this.mode = this.getOption('mode');
+			this.wrap();
+			this.addSpinner();
+			this.addControl();
+			this.addEvents();
+		},
 
- 		addControl: function(){
- 			this.playElement = $("<ins class='play-gif'>" + this.options.label + "</ins>");
- 			this.playElement.css('left', this.previewElement.width()/2 + this.playElement.width()/2);
- 			this.wrapper.append(this.playElement);
- 		},
+		wrap: function(){
+			this.previewElement.addClass('gifplayer-ready');
+			this.wrapper = this.previewElement.wrap("<div class='gifplayer-wrapper'></div>").parent();
+			this.wrapper.css('width', this.previewElement.width());
+			this.wrapper.css('height', this.previewElement.height());
+			this.previewElement.css('cursor','pointer');
+		},
 
- 		addEvents: function(){
- 			var gp=this;
- 			if(gp.options.autoLoad){
- 				gp.playElement.hide();
- 				gp.spinnerElement.show();
- 				gp.loadGif();
- 			}
- 			gp.playElement.on( 'click', function(e){
- 				$(this).hide();
- 				gp.spinnerElement.show();
- 				gp.loadGif();
- 				e.preventDefault();
-   				e.stopPropagation();
- 			});
- 			gp.previewElement.on( 'click', function(e){
- 				if(gp.playElement.is(':visible')){
-	 				gp.playElement.hide();
-	 				gp.spinnerElement.show();
-	 				gp.loadGif();
- 				}
- 				e.preventDefault();
-   				e.stopPropagation();
- 			});
- 			gp.spinnerElement.on('click', function(e){
- 				e.preventDefault();
-   				e.stopPropagation();
- 			});
- 		},
+		addSpinner: function(){
+			this.spinnerElement = $("<div class = 'spinner'></div>");
+			this.wrapper.append(this.spinnerElement);
+			this.spinnerElement.hide();
+		},
 
- 		loadGif: function(){
- 			if(!this.gifLoaded){
- 				this.enableAbort();
- 			}
- 			var gifSrc=this.getGifSrc();
- 			var gifWidth=this.previewElement.width();
- 			var gifHeight=this.previewElement.height();
- 			this.gifElement=$("<img src='" + gifSrc + "' width='"+ gifWidth + "' height=' "+ gifHeight +" '/>");
- 			var gp=this;
- 			this.gifElement.load( function(){
- 				gp.gifLoaded=true;
- 				gp.resetEvents();
- 				$(this).css('cursor','pointer');
+		getOption: function(option){
+			var dataOption = this.previewElement.data(option.toLowerCase());
+			if(dataOption != undefined && dataOption != ''){
+				return dataOption;
+			}else{
+				return this.options[option];
+			}
+		},
+
+		addControl: function(){
+			var label = this.getOption('label');
+			this.playElement = $("<ins class='play-gif'>" + label + "</ins>");
+			this.wrapper.append(this.playElement);
+			this.playElement.css('top', this.previewElement.height()/2 - this.playElement.height()/2);
+			this.playElement.css('left', this.previewElement.width()/2 - this.playElement.width()/2);
+		},
+
+		addEvents: function(){
+			var gp = this;
+			var playOn = this.getOption('playOn');
+
+			switch(playOn){
+				case 'click':
+					gp.playElement.on( 'click', function(e){
+						gp.previewElement.trigger('click');
+					});
+					gp.previewElement.on( 'click', function(e){
+						gp.loadAnimation();
+						e.preventDefault();
+						e.stopPropagation();
+					});
+					break;
+				case 'hover':
+					gp.previewElement.on( 'click mouseover', function(e){
+						gp.loadAnimation();
+						e.preventDefault();
+						e.stopPropagation();
+					});
+					break;
+				case 'auto':
+					console.log('auto not implemented yet');
+					break;
+				default:
+					console.log(playOn + ' is not accepted as playOn value.');
+			}
+		},
+
+		processScope: function(){
+			scope = this.getOption('scope');
+			if( scope ){
+				if(GifPlayer.scopes[scope]){
+					GifPlayer.scopes[scope].stopGif();
+				}
+				GifPlayer.scopes[scope] = this;
+			}
+		},
+
+		loadAnimation: function(){
+			this.processScope();
+
+			this.spinnerElement.show();			
+
+			if( this.mode == 'gif'){
+				this.loadGif();
+			}else if(this.mode == 'video'){
+				if(!this.videoLoaded){
+					this.loadVideo();
+				}else{
+					this.playVideo();
+				}
+
+			}
+			// Fire event onPlay
+			this.getOption('onPlay').call(this.previewElement);
+		},
+
+		stopGif: function(){
+			this.gifElement.hide();
+			this.previewElement.show();
+			this.playElement.show();
+			this.resetEvents();
+			this.getOption('onStop').call(this.previewElement);
+		},
+
+		getFile: function( ext ){
+			// Obtain the resource default path
+			var gif = this.getOption('gif');
+			if(gif != undefined && gif != ''){
+				return gif;
+			}else{
+				replaceString = this.previewElement.attr('src');
+
+				for (i = 0; i < this.supportedFormats.length; i++) {
+					pattrn = new RegExp( this.supportedFormats[i]+'$', 'i' );
+					replaceString = replaceString.replace( pattrn, ext );
+				}
+
+				return replaceString;
+			}
+		},
+
+		loadGif: function(){
+			var gp = this;
+
+			gp.playElement.hide();
+
+			if(!this.animationLoaded){
+				this.enableAbort();
+			}
+			var gifSrc = this.getFile('gif');
+			var gifWidth = this.previewElement.width();
+			var gifHeight = this.previewElement.height();
+			
+			this.gifElement=$("<img class='gp-gif-element' width='"+ gifWidth + "' height=' "+ gifHeight +" '/>");
+
+			var wait = this.getOption('wait');
+			if(wait){
+				//Wait until gif loads
+				this.gifElement.load( function(){
+					gp.animationLoaded = true;
+					gp.resetEvents();
+					gp.previewElement.hide();
+					gp.wrapper.append(gp.gifElement);
+					gp.spinnerElement.hide();
+					gp.getOption('onLoadComplete').call(gp.previewElement);
+				});
+			}else{
+				//Try to show gif instantly
+				gp.animationLoaded = true;
+				gp.resetEvents();
 				gp.previewElement.hide();
- 				gp.wrapper.append(gp.gifElement);
- 				gp.spinnerElement.hide();
- 				
- 				$(this).click( function(e){
- 					$(this).remove();
- 					gp.previewElement.show();
- 					gp.playElement.show();
- 					e.preventDefault();
- 					e.stopPropagation();
- 				});
+				gp.wrapper.append(gp.gifElement);
+				gp.spinnerElement.hide();
+			}
+			this.gifElement.css('cursor','pointer');
+			this.gifElement.css('position','absolute');
+			this.gifElement.css('top','0');
+			this.gifElement.css('left','0');
+			this.gifElement.attr('src', gifSrc);
+			this.gifElement.click( function(e){
+				$(this).remove();
+				gp.stopGif();
+				e.preventDefault();
+				e.stopPropagation();
 			});
- 			
- 		},
+			gp.getOption('onLoad').call(gp.previewElement);
+			
+		},
 
- 		enableAbort: function(){
- 			var gp = this;
- 			this.previewElement.click( function(e){
- 				gp.abortLoading(e);
- 			});
- 			this.spinnerElement.click( function(e){
- 				gp.abortLoading(e);
- 			});
- 		},
+		loadVideo: function(){
+			this.videoLoaded = true;
 
- 		abortLoading: function(e){
- 			this.spinnerElement.hide();
+			var videoSrcMp4 = this.getFile('mp4');
+			var videoSrcWebm = this.getFile('webm');
+			var videoWidth = this.previewElement.width();
+			var videoHeight = this.previewElement.height();
+
+			this.videoElement = $('<video class="gp-video-element" width="' + 
+				videoWidth + 'px" height="' + videoHeight + '" style="margin:0 auto;width:' + 
+				videoWidth + 'px;height:' + videoHeight + 'px;" autoplay="autoplay" loop="loop" muted="muted" poster="' + 
+				this.previewElement.attr('src') + '"><source type="video/mp4" src="' + 
+				videoSrcMp4 + '"><source type="video/webm" src="' + videoSrcWebm + '"></video>');
+
+			var gp = this;
+
+			var checkLoad = function(){
+				if(gp.videoElement[0].readyState === 4){
+					gp.playVideo();
+					gp.animationLoaded = true;
+				}else{
+					setTimeout(checkLoad, 100);
+				}
+			};
+
+			var wait = this.getOption('wait');
+			if(wait){
+				checkLoad();
+			}else{
+				this.playVideo();
+			}
+			
+			this.videoElement.on('click', function(){
+				if(gp.videoPaused){
+					gp.resumeVideo();
+				}else{
+					gp.pauseVideo();
+				}
+			});
+		},
+
+		playVideo: function(){
+			this.spinnerElement.hide();
+			this.previewElement.hide();
+			this.playElement.hide();
+
+			this.gifLoaded = true;
+			this.previewElement.hide();
+			this.wrapper.append(this.videoElement);
+			this.videoPaused = false;
+			this.videoElement[0].play();
+			this.getOption('onPlay').call(this.previewElement);
+		},
+
+		pauseVideo: function(){
+			this.videoPaused = true;
+			this.videoElement[0].pause();
+			this.playElement.show();
+			this.mouseoverEnabled = false;
+			this.getOption('onStop').call(this.previewElement);
+		},
+
+		resumeVideo: function(){
+			this.videoPaused = false;
+			this.videoElement[0].play();
+			this.playElement.hide();
+			this.getOption('onPlay').call(this.previewElement);
+		},
+
+		enableAbort: function(){
+			var gp = this;
+			this.previewElement.click( function(e){
+				gp.abortLoading(e);
+			});
+			this.spinnerElement.click( function(e){
+				gp.abortLoading(e);
+			});
+		},
+
+		abortLoading: function(e){
+			this.spinnerElement.hide();
 			this.playElement.show();
 			e.preventDefault();
 			e.stopPropagation();
@@ -120,33 +279,78 @@
 				ev.stopPropagation();
 			});
 			this.resetEvents();
- 		},
+			this.getOption('onStop').call(this.previewElement);
+		},
 
- 		resetEvents: function(){
- 			this.previewElement.off('click');
+		resetEvents: function(){
+			this.previewElement.off('click');
+			this.previewElement.off('mouseover');
 			this.playElement.off('click');
 			this.spinnerElement.off('click');
 			this.addEvents();
- 		},
+		}
 
- 		addSpinner: function(){
- 			this.wrapper.append(this.spinnerElement);
- 			this.spinnerElement.hide();
- 		}
+	};
 
- 	};
+	$.fn.gifplayer = function(options) {
 
- 	$.fn.gifplayer = function(options) {
- 		return this.each(function(){
- 			options = $.extend({}, $.fn.gifplayer.defaults, options);
- 			var gifplayer = new GifPlayer($(this), options);
- 			gifplayer.activate();
- 		});	
- 	};
+		// Check if we should operate with some method
+		if (/^(play|stop)$/i.test(options)) {
 
- 	$.fn.gifplayer.defaults = {
- 		label: 'gif',
- 		autoLoad: false
- 	};
+			return this.each( function(){
+				// Normalize method's name
+				options = options.toLowerCase();
+				if($(this).hasClass('gifplayer-ready')){
+					//Setup gifplayer object
+					var gp = new GifPlayer($(this), null);
+					gp.options = {};
+					gp.options = $.extend({}, $.fn.gifplayer.defaults, gp.options);
+					gp.wrapper = $(this).parent();
+					gp.spinnerElement = gp.wrapper.find('.spinner');
+					gp.playElement = gp.wrapper.find('.play-gif');
+					gp.gifElement = gp.wrapper.find('.gp-gif-element');
+					gp.videoElement = gp.wrapper.find('.gp-video-element');
+					gp.mode = gp.getOption('mode');
 
- })(jQuery);
+					switch(options){
+						case 'play':
+							gp.playElement.trigger('click');
+							break;
+						case 'stop':
+							if(!gp.playElement.is(':visible')){
+								if(gp.mode == 'gif'){
+									gp.stopGif();
+								}else if( gp.mode == 'video'){
+									gp.videoElement.trigger('click');
+								}
+							}
+							break;
+					}
+				}
+			});
+
+		}else{ //Create instance
+			return this.each(function(){
+				options = $.extend({}, $.fn.gifplayer.defaults, options);
+				var gifplayer = new GifPlayer($(this), options);
+				gifplayer.activate();
+			});
+		}	
+	};
+
+	$.fn.gifplayer.defaults = {
+		label: 'GIF',
+		playOn: 'click',
+		mode: 'gif',
+		gif: '',
+		mp4: '',
+		webm: '',
+		wait: false,
+		scope: false,
+		onPlay: function(){},
+		onStop: function(){},
+		onLoad: function(){},
+		onLoadComplete: function(){}
+	};
+	
+})(jQuery);
