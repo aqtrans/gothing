fa5892954ee846377faf6db856d941df704ec7a5
Jordan Anderson <me@jba.io>
2016-04-15 20:24:26 -0500

    Reversed sorting functions

--- main.go
+++ main.go
@@ -130,31 +130,31 @@ type ScreenshotByDate []*Screenshot
 
 func (a ScreenshotByDate) Len() int           { return len(a) }
 func (a ScreenshotByDate) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ScreenshotByDate) Less(i, j int) bool { return a[i].Created < a[j].Created }
+func (a ScreenshotByDate) Less(i, j int) bool { return a[i].Created > a[j].Created }
 
 type ImageByDate []*Image
 
 func (a ImageByDate) Len() int           { return len(a) }
 func (a ImageByDate) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ImageByDate) Less(i, j int) bool { return a[i].Created < a[j].Created }
+func (a ImageByDate) Less(i, j int) bool { return a[i].Created > a[j].Created }
 
 type PasteByDate []*Paste
 
 func (a PasteByDate) Len() int           { return len(a) }
 func (a PasteByDate) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a PasteByDate) Less(i, j int) bool { return a[i].Created < a[j].Created }
+func (a PasteByDate) Less(i, j int) bool { return a[i].Created > a[j].Created }
 
 type FileByDate []*File
 
 func (a FileByDate) Len() int           { return len(a) }
 func (a FileByDate) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a FileByDate) Less(i, j int) bool { return a[i].Created < a[j].Created }
+func (a FileByDate) Less(i, j int) bool { return a[i].Created > a[j].Created }
 
 type ShortByDate []*Shorturl
 
 func (a ShortByDate) Len() int           { return len(a) }
 func (a ShortByDate) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ShortByDate) Less(i, j int) bool { return a[i].Created < a[j].Created }
+func (a ShortByDate) Less(i, j int) bool { return a[i].Created > a[j].Created }
 
 func init() {
 
