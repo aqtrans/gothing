d602a49b9470d9e05edbbebd56f130d7f007b1a4
Jordan Anderson <me@jba.io>
2018-02-19 02:28:57 -0600

    Revert notification bar changes.

--- main.go
+++ main.go
@@ -176,7 +176,7 @@ type Page struct {
 	UN       string
 	IsAdmin  bool
 	Token    template.HTML
-	FlashMsg template.HTML
+	FlashMsg string
 	MainTLD  string
 }
 
@@ -382,21 +382,19 @@ func loadPage(title string, w http.ResponseWriter, r *http.Request) (*Page, erro
 	msg := auth.GetFlash(r.Context())
 	token := csrf.TemplateField(r)
 
-	var message template.HTML
+	var message string
 	if msg != "" {
-		message = template.HTML(`
-			<div class="notification anim active" id="notification">
-			<p>` + msg + `
-			<button class="close-button" type="button" onclick="notif()">
-			<div class="svg-icon"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-			<title>cross</title>
-			<path d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path>
-			</svg></div>
-			</button></p>
-			</div>
-		`)
+		message = `
+			<div class="alert callout" data-closable>
+			<h5>Alert!</h5>
+			<p>` + msg + `</p>
+			<button class="close-button" aria-label="Dismiss alert" type="button" data-close>
+				<span aria-hidden="true">&times;</span>
+			</button>
+			</div>			
+        `
 	} else {
-		message = template.HTML("")
+		message = ""
 	}
 
 	return &Page{
